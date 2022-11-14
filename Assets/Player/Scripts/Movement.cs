6885d91d10ca4f665339d2559693218194e67c55
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-11-13 21:28:52 -0300

    🎈 design pattern

--- Assets/Player/Scripts/Movement.cs
+++ Assets/Player/Scripts/Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     public float move_speed = 5f;
 
