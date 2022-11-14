85dba092c1209437dd1c1b5b2c780eee5ab3becf
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-11-14 00:53:42 -0300

    🌾 plants and islands

---
+++ Assets/Tilesets/WorldTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="WorldTileSet" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
+ <image source="../World/WorldTileSet.png" width="384" height="384"/>
+</tileset>
