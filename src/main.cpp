4a0b08e058a7f993365331f72eab2bd9ccc379e7
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-04-17 22:51:35 -0300

    ⚽ player

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
-#include <string>
 
 #include "raylib.h"
 
 int main() {
 
   const int screenWidth = 512;
-  const int screenHeigt = 512;
+  const int screenHeight = 512;
 
-  InitWindow(screenWidth, screenHeigt, "Game");
+  InitWindow(screenWidth, screenHeight, "Game");
 
   SetTargetFPS(60);
 
   Texture2D background = LoadTexture("assets/map.png");
+
+  Texture2D ball = LoadTexture("assets/ball.png");
+
+  int ballWidth = ball.width;
+  int ballHeight = ball.height;
+
+  // Source rectangle (part of the texture to use for drawing)
+  Rectangle sourceRec = { 0.0f, 0.0f, (float)ballWidth, (float)ballHeight };
+
+  // Destination rectangle (screen rectangle where drawing part of texture)
+  Rectangle destRec = { screenWidth/2.0f, screenHeight/2.0f, ballWidth*2.0f, ballHeight*2.0f };
+
+  // Origin of the texture (rotation/scale point), it's relative to destination rectangle size
+  Vector2 origin = { (float)ballWidth, (float)ballHeight };
+
 
   while(!WindowShouldClose()) {
 
@@ -21,6 +35,7 @@ int main() {
     BeginDrawing();
 
     DrawTextureEx(background, (Vector2){ 0.0f, 0.0f }, 0.0f, 8.0f, WHITE);
+    DrawTexturePro(ball, sourceRec, destRec, origin, 0.f, WHITE);
 
     EndDrawing();
   }
