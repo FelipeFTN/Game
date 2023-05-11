479ebc0a644db41c91d19c36321be6b37d032eaa
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-05-11 12:11:12 -0300

    🦜 runner

--- Game.sh
+++ Game.sh
@@ -1,5 +1,14 @@
+# Clear any previous installation
 make clean
+
+# Install dependencies
 make install
-export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
+
+# Compile the Game
 make
+
+# Export library to path
+export LD_LIBRARY_PATH=./lib/raylib/lib:$LD_LIBRARY_PATH
+
+# Run Game
 ./bin/Game
